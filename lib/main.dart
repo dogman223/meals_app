f006e61670926f14b30d1ebb70c6ffe27a23731a
Krzysztof Aniołkowski <kjaniol94@gmail.com>
2023-04-04 13:43:12 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import './categories_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'KrzysztofMeals',
+      theme: ThemeData(
+        primarySwatch: Colors.pink,
+        canvasColor: Color.fromRGBO(225, 254, 229, 1),
+        fontFamily: 'Raleway',
+        textTheme: ThemeData.light().textTheme.copyWith(
+              titleLarge:
+                  TextStyle(fontSize: 24, fontFamily: 'RobotoCondensed'),
+            ),
+      ),
+      home: CategoriesScreen(),
+    );
+  }
+}
